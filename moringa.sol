3a8736aba5454bcdd0c896d9bc52e7a0d7ee7606
Hijrah Moring <108036434+HijrahMoringa@users.noreply.github.com>
2022-06-24 16:05:44 +0800

    Remove mint token function

--- moringa.sol
+++ moringa.sol
@@ -527,23 +527,6 @@ contract BEP20Token is Context, IBEP20, Ownable {
     emit Transfer(sender, recipient, amount);
   }
 
-  /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-   * the total supply.
-   *
-   * Emits a {Transfer} event with `from` set to the zero address.
-   *
-   * Requirements
-   *
-   * - `to` cannot be the zero address.
-   */
-  function _mint(address account, uint256 amount) internal {
-    require(account != address(0), "BEP20: mint to the zero address");
-
-    _totalSupply = _totalSupply.add(amount);
-    _balances[account] = _balances[account].add(amount);
-    emit Transfer(address(0), account, amount);
-  }
-
   /**
    * @dev Destroys `amount` tokens from `account`, reducing the
    * total supply.
